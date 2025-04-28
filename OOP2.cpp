de574e708a314d3f273aa745194d9b6e33707a2e
220FasyaTriNugroho <fasya.tri.ft24@mail.umy.ac.id>
2025-04-28 19:00:05 +0700

    membuat persegi panjang

---
+++ OOP2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class persegipanjang
+{
+public:
+    float panjang, lebar, luas;
+    void inputdata()
+    {
+        cout << "Input Panjang = ";
+        cin >> panjang;
+        cout << "Input Lebar = ";
+        cin >> lebar;
+    }
+    void menghitungluas()
+    {
+        luas = panjang*lebar;
+        cout << "Luas Persegi Panjang = " << luas << endl;
+    }
+};
+
+int main()
+{
+}
